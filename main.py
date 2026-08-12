cafb09b975e10cc8432ebed2286d26aa55511397
Oleksandr <yeliseevalex20@gmail.com>
2026-08-12 20:41:25 +0400

    Add lines in new.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ print("Hello world")
 
 print("Hello world!!!!")
 
-print("New line")
+print("New line 2")
---
+++ new.py
@@ -0,0 +1,6 @@
+print("Hello world!!!!")
+print(5+5)
+print(5+5)
+print(5+5)
+print(5+5)
+print(5+5)
